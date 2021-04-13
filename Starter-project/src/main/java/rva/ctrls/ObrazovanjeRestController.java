87901ed39e1e360b7e77e76e4fa820d6426a7509
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-13 02:37:16 +0200

    Added SQL scripts for creating and seeding

--- Starter-project/src/main/java/rva/ctrls/ObrazovanjeRestController.java
+++ Starter-project/src/main/java/rva/ctrls/ObrazovanjeRestController.java
@@ -3,8 +3,15 @@ package rva.ctrls;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rva.jpa.Obrazovanje;
@@ -15,6 +22,9 @@ public class ObrazovanjeRestController {
 	
 	@Autowired
 	private ObrazovanjeRepository obrazovanjeRepository;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	@GetMapping("obrazovanje")
 	public Collection<Obrazovanje> getObrazovanja() {
@@ -29,5 +39,37 @@ public class ObrazovanjeRestController {
 	@GetMapping("obrazovanjeNaziv/{naziv}")
 	public Collection<Obrazovanje> getObrazovanjeByNaziv(@PathVariable("naziv") String naziv) {
 		return obrazovanjeRepository.findByNazivContainingIgnoreCase(naziv);
+	}
+	
+	@PostMapping("obrazovanje")
+	public ResponseEntity<Obrazovanje> insertObrazovanje(@RequestBody Obrazovanje obrazovanje) {
+		if (!obrazovanjeRepository.existsById(obrazovanje.getId())) {
+			obrazovanjeRepository.save(obrazovanje);
+			return new ResponseEntity<Obrazovanje>(HttpStatus.OK);
+		}
+		return new ResponseEntity<Obrazovanje>(HttpStatus.CONFLICT);
+	}
+	
+	@PutMapping("obrazovanje")
+	public ResponseEntity<Obrazovanje> updateObrazovanje(@RequestBody Obrazovanje obrazovanje) {
+		if (!obrazovanjeRepository.existsById(obrazovanje.getId())) {
+			return new ResponseEntity<Obrazovanje>(HttpStatus.CONFLICT);
+		}
+		obrazovanjeRepository.save(obrazovanje);
+		return new ResponseEntity<Obrazovanje>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("obrazovanje/{id}")
+	public ResponseEntity<Obrazovanje> deleteObrazovanje(@PathVariable Integer id) {
+		if (!obrazovanjeRepository.existsById(id)) {
+			return new ResponseEntity<Obrazovanje>(HttpStatus.NO_CONTENT);
+		}
+		obrazovanjeRepository.deleteById(id);
+		// Used only in purpose of testing
+		if (id == -100) {
+			jdbcTemplate.execute(" INSERT INTO \"obrazovanje\" (\"id\", \"naziv\", \"stepen_strucne_spreme\", \"opis\") "
+					+ "VALUES (-100, 'FTN', 'V', 'Test');");
+		}
+		return new ResponseEntity<Obrazovanje>(HttpStatus.OK);
 	}
 }
